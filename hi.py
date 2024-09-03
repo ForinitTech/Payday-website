18c0400268db915c8b74453348fa42d27d1df4aa
Tharaniesh <99646275+Tharaniesh3@users.noreply.github.com>
2024-09-04 00:28:50 +0530

    20th commit

--- hi.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://us-central1-payday-8ab25.cloudfunctions.net/appLinkCaller'
-data = {'updateCount': True}
-response = requests.post(url, json=data)
-
-print(response.status_code)
-print(response.json())
